e9c9c4a9773ef33d2adf9c589ec1dcfea30c01cc
Cliff Wu <z800201002005@gmail.com>
2018-04-09 00:54:30 +0800

    Add onClick action for button on login and sign up activity

--- app/src/main/java/tw/org/edo/gpssmartcane/LoginActivity.java
+++ app/src/main/java/tw/org/edo/gpssmartcane/LoginActivity.java
@@ -5,12 +5,15 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Context mContext = this;
     private TextView mSignUpTextView;
+    private Button mLoginButton;
+    private Button.OnClickListener mLoginButtonListen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,14 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         mSignUpTextView = findViewById(R.id.textViewSignUp);
+        mLoginButton = findViewById(R.id.buttonLogin);
+        mLoginButtonListen =  new Button.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        };
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
@@ -28,5 +39,6 @@ public class LoginActivity extends AppCompatActivity {
         };
 
         mSignUpTextView.setOnClickListener(listener);
+        mLoginButton.setOnClickListener(mLoginButtonListen);
     }
 }
--- app/src/main/java/tw/org/edo/gpssmartcane/SignUpActivity.java
+++ app/src/main/java/tw/org/edo/gpssmartcane/SignUpActivity.java
@@ -2,12 +2,28 @@ package tw.org.edo.gpssmartcane;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private Button mSignUpButton;
+    private Button.OnClickListener mSignUpButtonListen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        mSignUpButton = findViewById(R.id.buttonSignUp);
+        mSignUpButtonListen =  new Button.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        };
+
+        mSignUpButton.setOnClickListener(mSignUpButtonListen);
     }
 }
