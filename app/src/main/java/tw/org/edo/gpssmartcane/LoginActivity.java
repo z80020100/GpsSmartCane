e0c26597b3e273785efcac85f372ae6c20f30bf1
Cliff Wu <z800201002005@gmail.com>
2018-04-09 00:24:54 +0800

    Add activity for sign up

--- app/src/main/java/tw/org/edo/gpssmartcane/LoginActivity.java
+++ app/src/main/java/tw/org/edo/gpssmartcane/LoginActivity.java
@@ -1,13 +1,32 @@
 package tw.org.edo.gpssmartcane;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Context mContext = this;
+    private TextView mSignUpTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mSignUpTextView = findViewById(R.id.textViewSignUp);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, SignUpActivity.class);
+                startActivity(intent);
+            }
+        };
+
+        mSignUpTextView.setOnClickListener(listener);
     }
 }
