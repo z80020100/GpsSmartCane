ec0b7d663a003cf4f035a3b9c5b0292e27b81a95
Cliff Wu <z800201002005@gmail.com>
2018-05-23 01:13:48 +0800

    Implement user account add function

--- app/src/main/java/tw/org/edo/gpssmartcane/SignUpActivity.java
+++ app/src/main/java/tw/org/edo/gpssmartcane/SignUpActivity.java
@@ -1,29 +1,125 @@
 package tw.org.edo.gpssmartcane;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
+
+import static tw.org.edo.gpssmartcane.Constant.NAME_REGISTER_EMAIL;
+import static tw.org.edo.gpssmartcane.Constant.NAME_REGISTER_PASSWORD;
+import static tw.org.edo.gpssmartcane.Constant.RESULT_REGISTER_FAIL_EXISTS;
+import static tw.org.edo.gpssmartcane.Constant.RESULT_REGISTER_FAIL_UNKNOWN;
+import static tw.org.edo.gpssmartcane.Constant.RESULT_REGISTER_SUCCESS;
+import static tw.org.edo.gpssmartcane.Constant.URL_REGISTER;
 
 public class SignUpActivity extends AppCompatActivity {
+    final private  String TAG = this.getClass().getSimpleName();
+
+    private Context mContext = this;
+    private EditText mUserNameEditText, mPasswordEditText,
+            mPhoneEditText, mCaneIdEdidText;
+    private String mUserName, mPassword;
+
+    private TextView mPhoneTextView, mCaneIdTextView;
 
     private Button mSignUpButton;
     private Button.OnClickListener mSignUpButtonListen;
+
+    private Runnable mHttpRunnable;
+    private Thread mHttpThread;
+    private int mRegisterResult = RESULT_REGISTER_FAIL_UNKNOWN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        mUserNameEditText = findViewById(R.id.editTextUserName);
+        mPasswordEditText = findViewById(R.id.editTextPassword);
+        mPhoneEditText = findViewById(R.id.editTextPhone);
+        mCaneIdEdidText = findViewById(R.id.editTextCaneId);
+
+        // Only for develop
+        mUserNameEditText.setText("test@gmail.com.1");
+        mPasswordEditText.setText("z");
+
+        mPhoneTextView = findViewById(R.id.textViewPhone);
+        mCaneIdTextView = findViewById(R.id.textViewCaneId);
+
+        mPhoneEditText.setVisibility(View.GONE);
+        mCaneIdEdidText.setVisibility(View.GONE);
+        mPhoneTextView.setVisibility(View.GONE);
+        mCaneIdTextView.setVisibility(View.GONE);
+
         mSignUpButton = findViewById(R.id.buttonSignUp);
         mSignUpButtonListen =  new Button.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                finish();
+                //finish();
+                mUserName = mUserNameEditText.getText().toString();
+                mPassword = mPasswordEditText.getText().toString();
+                if(mUserName.length() > 0 && mPassword.length() > 0){
+                    Log.i(TAG, "mUserName = " + mUserName + ", mPassword = " + mPassword);
+                    mHttpRunnable = new Runnable(){
+                        @Override
+                        public void run() {
+                            String url = URL_REGISTER;
+                            ArrayList<NameValuePair> params = new ArrayList<>();
+                            params.add(new BasicNameValuePair(NAME_REGISTER_EMAIL, mUserName));
+                            params.add(new BasicNameValuePair(NAME_REGISTER_PASSWORD, mPassword));
+                            String return_data = DBConnector.executeQuery(params, url);
+                            Log.i(TAG, "return_data = " + return_data);
+                            mRegisterResult = Character.getNumericValue(return_data.charAt(0));
+                            Log.i(TAG, "mRegisterResult = " + mRegisterResult);
+
+                            if(mRegisterResult != RESULT_REGISTER_SUCCESS){
+                                mRegisterResult = mRegisterResult * Character.getNumericValue(return_data.charAt(1));
+                            }
+
+                            if(mRegisterResult == RESULT_REGISTER_SUCCESS){
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Utility.makeTextAndShow(mContext, "註冊成功", 2);
+                                    }
+                                });
+                            }
+                            else if(mRegisterResult == RESULT_REGISTER_FAIL_EXISTS){
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Utility.makeTextAndShow(mContext, "註冊失敗：帳號已存在", 2);
+                                    }
+                                });
+                            }
+                            else{
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Utility.makeTextAndShow(mContext, "註冊失敗：請洽系統管理員", 2);
+                                    }
+                                });
+                            }
+                        }
+                    };
+                    mHttpThread = new Thread(mHttpRunnable);
+                    mHttpThread.start();
+                }
+                else{
+                    Utility.makeTextAndShow(mContext, "使用者名稱或密碼不可空白", 2);
+                }
             }
         };
-
         mSignUpButton.setOnClickListener(mSignUpButtonListen);
     }
 }
